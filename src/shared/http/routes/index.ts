d664aa69e8bb5e526aa8efc55dca6e1faf33dcc6
Mauriciiow <mauricioliveira639@gmail.com>
2022-05-06 19:19:43 -0300

    corrigindo local dos users

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import productsRouter from "@modules/products/routes/products.routes";
-import usersRouter from "@modules/products/users/routes/users.routes";
+import usersRouter from "@modules/users/routes/users.routes";
 
 const routes = Router();
 
