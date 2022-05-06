33232bcd70ab86054efa37cec49925c8a4fce4a5
Mauriciiow <mauricioliveira639@gmail.com>
2022-05-06 01:17:10 -0300

    adicionando rota de usuario nas rotas globais

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productsRouter from "@modules/products/routes/products.routes";
+import usersRouter from "@modules/products/users/routes/users.routes";
 
 const routes = Router();
 
 routes.use('/products', productsRouter)
+routes.use('/users', usersRouter)
 
 
 export default routes
